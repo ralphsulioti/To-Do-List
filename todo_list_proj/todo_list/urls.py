8c57e3b9a6dc112925e81f49e793f70cb5b6712a
ralphsulioti <ralph.sulioti@gmail.com>
2024-03-22 14:50:28 -0400

    Added details, add, delete functionalities for items in a list
    User can now add, delete, and view details of an item in a list.

--- todo_list_proj/todo_list/urls.py
+++ todo_list_proj/todo_list/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('logout', views.logout_view),
-    path('<int:list_id>/', views.list_view),
+    path('<int:list_id>/', views.list_view, name='list_view'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item'),
+    path('add/<int:list_id>/', views.add_item, name='add_item'),
+    path('details/<int:item_id>/', views.item_details, name='item_details'),
 ]
--- todo_list_proj/todo_list/views.py
+++ todo_list_proj/todo_list/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib.auth import logout
+from django.utils import timezone
 from .models import List, Item, User
 
 # Create your views here.
@@ -12,6 +13,25 @@ def logout_view(request):
 
 
 def list_view(request, list_id):
-    list_obj = List.objects.get(pk=list_id)
-    list_items = list_obj.item_set.all()  # Accessing related items through the foreign key relationship
-    return render(request, 'list.html', {'list_items': list_items})
+    list_obj = get_object_or_404(List, pk=list_id)
+    list_items = Item.objects.filter(list_id=list_obj)
+    return render(request, 'list.html', {'list_obj': list_obj, 'list_items': list_items})
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == 'POST':
+        item.delete()
+    return redirect('list_view', list_id=item.list_id.id)
+
+def add_item(request, list_id):
+    if request.method == 'POST':
+        list_obj = get_object_or_404(List, pk=list_id)
+        item_descr = request.POST.get('item_descr')
+        new_item = Item.objects.create(list_id=list_obj, item_descr=item_descr, item_date_added=timezone.now(), item_done=False)
+    return redirect('list_view', list_id=list_id)
+
+def item_details(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    list_name = item.list_id.list_name
+    return render(request, 'item_details.html', {'item': item, 'list_name': list_name})
